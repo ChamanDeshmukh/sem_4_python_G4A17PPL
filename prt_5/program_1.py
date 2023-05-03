3592069a2009b5c9e8511c078f78a665908570c1
Chaman Deshmukh <deshmukhck7@gmail.com>
2023-05-03 06:34:11 +0530

    Added 5th Practical

---
+++ prt_5/program_1.py
@@ -0,0 +1,17 @@
+class points:
+    def __init__(self):
+        self.marks[]
+    
+    def acc(self):
+        print("Enter Marks : ")
+        for i in range(3):
+            marks.append(("Subject {} : ".format(i)))
+
+    def display(self):
+        for i in marks:
+            print("Marks in Subject {} : {}".format(i,marks[i]))
+
+obj = points()
+
+obj.acc()
+obj.display()
